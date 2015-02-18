2bcc2da0f0ed7ed62c6ec2fdc3d300c23ffc9b8e
Christian Chandler <247webdev+github@googlemail.com>
2015-02-18 00:26:09 -0800

    commiting HW lab. Problems not resolved: delete student not removing from DB. binding.pry shows correct data passed from form to route delete '/squads/:squad_id/students/:student_id' not sure if app.rb line 105 doing job as expected. But, at binding.pry on line 104, params[:student_id] is correct data.

--- models/student.rb
+++ models/student.rb
@@ -1,5 +1,59 @@
 class Student
+  attr_reader :id
+  attr_accessor :squad_id, :name, :age, :spirit_animal
+
+  def initialize params, existing=false
+    @id = params["id"]
+    @squad_id = params["squad_id"]
+    @name = params["name"]
+    @age = params["age"]
+    @spirit_animal = params["spirit_animal"]
+    @existing = existing
+  end
+
+  def existing?
+    @existing
+  end
+
+  # should maintain a db connection
   def self.conn= connection
     @conn = connection
   end
+
+  def self.conn
+    @conn
+  end
+
+  # should return a list of students
+  def self.all
+    @conn.exec("SELECT * FROM students")
+  end
+
+  # should return a student by id
+  # or nil if not found
+  def self.find id
+    new @conn.exec('SELECT * FROM students WHERE id = ($1)', [ id ] )[0], true
+  end
+
+  def students
+    Student.conn.exec("SELECT * FROM students WHERE squad_id = ($1)", [id])
+  end
+
+  def save
+    if existing?
+      Student.conn.exec('UPDATE students SET squad_id=$1, name=$2, age=$3, spirit_animal=$4 WHERE id = $5', [ squad_id, name, age, spirit_animal, id ] )
+    else
+      Student.conn.exec('INSERT INTO students (squad_id, name, age, spirit_animal) values ($1, $2, $3, $4)', [ squad_id, name, age, spirit_animal ] )
+    end
+  end
+
+  def self.create params
+    new(params).save
+  end
+
+  def destroy
+    Student.conn.exec('DELETE FROM students WHERE squad_id = $1', [ id ] )
+    Student.conn.exec('DELETE FROM squads WHERE id = $1', [ id ] )
+  end
+
 end
